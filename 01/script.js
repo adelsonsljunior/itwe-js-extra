e3a1657aeebf1b3e8c53a9299b2c51c53186c17a
adelsonsljunior <junioradelsonst@gmail.com>
2024-08-05 20:22:11 -0300

    chore: parseInt in 01

--- 01/script.js
+++ 01/script.js
@@ -1,10 +1,10 @@
 function mostrarResultado() {
 
-    const numero1 = document.getElementById("numero1").value;
-    const numero2 = document.getElementById("numero2").value;
-    const numero3 = document.getElementById("numero3").value;
-    const numero4 = document.getElementById("numero4").value;
-    const numero5 = document.getElementById("numero5").value;
+    const numero1 = parseInt(document.getElementById("numero1").value);
+    const numero2 = parseInt(document.getElementById("numero2").value);
+    const numero3 = parseInt(document.getElementById("numero3").value);
+    const numero4 = parseInt(document.getElementById("numero4").value);
+    const numero5 = parseInt(document.getElementById("numero5").value);
 
     const resultado = maiorMenor(numero1, numero2, numero3, numero4, numero5);
     document.getElementById("resultado").textContent = `O maior valor é ${resultado.maior} e o menor valor é ${resultado.menor}`;
